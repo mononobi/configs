89403c0a101ff3bdcc324b6e77f9c6f85a848636
mono <5990186+mononobi@users.noreply.github.com>
2025-06-06 23:58:44 +0200

    Update python install scripts

--- linux/install/python/python3-is-python/script.sh
+++ linux/install/python/python3-is-python/script.sh
@@ -1,2 +1,2 @@
-sudo ln -s /usr/bin/python3.9 /usr/bin/python
-sudo ln -s /usr/bin/python3.9-config /usr/bin/python-config
+sudo ln -s /usr/bin/python3.13 /usr/bin/python
+sudo ln -s /usr/bin/python3.13-config /usr/bin/python-config
