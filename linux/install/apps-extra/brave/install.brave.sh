b3254acaf47484bda6cea9c0efcd85284460ed46
mono <5990186+mononobi@users.noreply.github.com>
2026-09-20 00:25:26 +0200

    Add Brave browser keyring installation to setup script

--- linux/install/apps-extra/brave/install.brave.sh
+++ linux/install/apps-extra/brave/install.brave.sh
@@ -45,8 +45,8 @@ is_installed "brave-browser" --name "Brave Browser" && exit 0
 
 echo "[+] Starting installation/setup for brave..."
 
-conditional_apt_update
 require_app ca-certificates curl gnupg
+
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSLo /etc/apt/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 sudo chmod 644 /etc/apt/keyrings/brave-browser-archive-keyring.gpg
